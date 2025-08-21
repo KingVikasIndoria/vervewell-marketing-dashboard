0834534baae81c3fb15ce4ce287d4d4b6d70633c
KingVikasIndoria <vikas.indoria@bluestone>
2025-08-21 23:51:40 +0530

    Fix: Correct build directory path - change dist to build

--- server/index.js
+++ server/index.js
@@ -660,11 +660,11 @@ app.get('/api/data/summary', (_req, res) => {
 // ---------------------------
 // Serve static files from the React build
 // ---------------------------
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 // ---------------------------
